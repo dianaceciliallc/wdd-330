e6dd14acd7a89884acc9679d67a50d4bc34f5f10
dianaceciliallc <dllerenacastro@gmail.com>
2026-09-19 15:59:49 -0500

    Done w02 individual activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,11 @@
 import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
 const productList = document.querySelector(".product-list");
 
 const dataSource = new ProductData("tents");
+const productListView = new ProductList("tents", dataSource, productList);
 
 window.addEventListener("DOMContentLoaded", async () => {
-  const products = await dataSource.getData();
-  products.forEach((product) => {
-    const productElement = document.createElement("li");
-    productElement.classList.add("product-card");
-    productElement.innerHTML = `
-        <a href="product_pages/index.html?product=${product.Id}">
-            <img
-            src=${product.Image}
-            alt=${product.Name}
-            />
-            <h3 class="card__brand">${product.Brand.Name}</h3>
-            <h2 class="card__name">${product.NameWithoutBrand}</h2>
-            <p class="product-card__price">$${product.ListPrice}</p>
-        </a>
-    `;
-    productList.appendChild(productElement);
-  });
+  await productListView.init();
 });
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -27,3 +27,11 @@ export function getParam(param) {
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin" , clear = false) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  const productElements = list.map((product) => templateFn(product))
+  parentElement.insertAdjacentHTML(position, productElements.join(''));
+}
